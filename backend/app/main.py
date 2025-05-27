76b6a556cdd6797017974979cdc0fe0ff888cf83
Tanay-Bhatt <tanayb700@gmail.com>
2025-05-28 04:16:17 +0530

    Added CORS Fallback

--- backend/app/main.py
+++ backend/app/main.py
@@ -18,7 +18,7 @@ from app.socket_server import sio_app
 
 api_app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
